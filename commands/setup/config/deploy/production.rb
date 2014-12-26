101af5b6c83a3257cc70ef4f5f8306697f82216d
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-12-26 19:43:03 +0100

    Fix setting up database

--- commands/setup/config/deploy/production.rb
+++ commands/setup/config/deploy/production.rb
@@ -6,16 +6,23 @@ PARAMS.app_server_addresses.each_with_index do |address, i|
     server(:local, :roles => ["app"])
   else
     uri = URI.parse("scheme://#{address}")
+
     hostname = uri.hostname.dup
     hostname << ":#{uri.port}" if uri.port
+
+    roles = ["app"]
+    if address == PARAMS.db_server_address
+      roles << "db"
+    end
+
     server(hostname,
       :user => uri.user || "root",
-      :roles => ["app"],
+      :roles => roles,
       :primary => i == 0)
   end
 end
 
-if PARAMS.db_server_address
+if PARAMS.db_server_address && !PARAMS.app_server_addresses.include?(PARAMS.db_server_address)
   if PARAMS.db_server_address == "localhost"
     server(:local, :roles => ["db"])
   else
